1592fc0e8602cfb56b56d4f15918cb950e7898fd
pydi_chandini <PYDI.CHANDINI@ORACLE.COM>
2021-05-24 16:14:51 +0530

    ICF final

--- src/main/java/com/icf/spring/app/employees/DemoApplication.java
+++ src/main/java/com/icf/spring/app/employees/DemoApplication.java
@@ -18,3 +18,17 @@ public String hello(@RequestParam(value = "name", defaultValue = "World") String
 return String.format("Hello %s!", name);
 }
 }
+
+
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+//	}
+//
+//}
